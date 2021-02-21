0fbf2f456c2571ade39b64277af209ef34f6fde5
claire ogesen <claire.ogesen.co@gmail.com>
2021-02-21 13:09:17 -0700

    it works!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,29 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (event) { 
+var taskFormHandler = function(event) { 
   event.preventDefault();
-var taskNameInput = document.querySelector("input[name='task-name]").value
+var taskNameInput = document.querySelector("input[name='task-name']").value;
 var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-// create list item
+//ask bcs had me change it to get elementby ID
+//var taskNameInput = document.getElementById("taskName").value;
+//var taskTypeInput = document.getElementById("taskType").value;
+
+//console.log(taskTypeInput, taskNameInput)
+
+//package up data as an oject
+var taskDataObj = {
+  name: taskNameInput,
+  type: taskTypeInput
+
+  }; 
+
+  createTaskE1(taskDataObj);
+};
+
+  var createTaskE1 = function (taskDataObj){
+    // create list item
 var listItemE1 = document.createElement("li");
 listItemE1.className = "task-item";
 
@@ -15,17 +32,15 @@ var taskInfoE1 = document.createElement("div");
 taskInfoE1.className = "task-info";
 
 // add HTML content to div
-taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 listItemE1.appendChild(taskInfoE1);
 
 
 // add entire list item to list
 tasksToDoE1.appendChild(listItemE1);
 
-console.dir(listItemE1); 
- 
-  }; 
+  }
 
- formE1.addEventListener("submit", createTaskHandler);
+ formE1.addEventListener("submit", taskFormHandler);
 
   
